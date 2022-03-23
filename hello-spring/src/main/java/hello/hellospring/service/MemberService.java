5d181f3b3a4745480646e9f27a4cfff2cffe16f8
ChanYoung <cyjoungg@gmail.com>
2022-03-23 18:48:07 +0900

    AOP

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -23,11 +23,20 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원X
-        validDuplicateMember(member);
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            // 같은 이름이 있는 중복 회원X
+            validDuplicateMember(member);
+
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validDuplicateMember(Member member) {
